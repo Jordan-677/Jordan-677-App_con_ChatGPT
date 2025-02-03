71704447391950b3ba2bf1b1afa9ca1a73e37f65
Jordan-677 <159493664+Jordan-677@users.noreply.github.com>
2025-02-03 16:16:02 -0500

    Update Taller08_00_JordanS.py

--- Taller08_00_JordanS.py
+++ Taller08_00_JordanS.py
@@ -9,17 +9,26 @@ from scipy import stats
 # Título de la aplicación
 st.title("Aplicación para Visualizar Datos de Deforestación")
 
-# Opción para cargar archivo
+# Opciones de carga
 st.sidebar.header("Opciones")
 cargar_datos = st.sidebar.button("Cargar archivo")
+cargar_por_url = st.sidebar.button("Cargar por URL")
 
 @st.cache_data
 def cargar_archivo():
-    """Carga el archivo CSV y muestra el contenido."""
+    """Carga el archivo CSV desde una URL fija y muestra el contenido."""
     url = "https://raw.githubusercontent.com/gabrielawad/programacion-para-ingenieria/refs/heads/main/archivos-datos/aplicaciones/deforestacion.csv"
     df = pd.read_csv(url)
     st.write("### Vista Previa del Archivo")
     st.dataframe(df)
 
+@st.cache_data
+def cargar_archivo_por_url(url):
+    """Carga un archivo CSV desde una URL ingresada y muestra el contenido."""
+    df = pd.read_csv(url)
+    st.write("### Vista Previa del Archivo")
+    st.dataframe(df)
+
 cargar_datos and cargar_archivo()
+cargar_por_url and st.text_input("Ingrese la URL del archivo CSV", key="url_input") and cargar_archivo_por_url(st.session_state.url_input)
 
